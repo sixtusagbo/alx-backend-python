a280b217f1b395383db4b4907b67f3def28655fd
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-08-15 05:46:06 +0100

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains a function that converts a float to a string"""
+
+
+def to_str(n: float) -> str:
+    """Convert a float to a string"""
+    return str(n)
