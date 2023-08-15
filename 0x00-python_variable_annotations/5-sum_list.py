edbf9766eea28fe5b3fe92260f9e4d74c4895904
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-08-15 06:07:59 +0100

    Update sum_list to be float specific

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""This module contains a function that sums up a list of numbers"""
+"""This module contains a function that sums up a list of floats"""
+from typing import List
 
 
-def sum_list(input_list: list) -> int | float:
+def sum_list(input_list: List[float]) -> float:
     """Return the sum of a list of numbers"""
     sum = 0
 
