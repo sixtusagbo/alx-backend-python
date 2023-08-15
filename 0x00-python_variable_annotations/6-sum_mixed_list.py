30acc91a9fe83fb941988b787ab8f5cee65d4306
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-08-15 06:14:37 +0100

    Complex types - mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module contains a function that sums up integers and floats"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Return sum of the numbers in a list as a float"""
+    sum = 0
+
+    for num in mxd_lst:
+        sum += num
+
+    return sum
