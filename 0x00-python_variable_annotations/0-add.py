d587de9f4f7311684654c1ffdf54841dbb589397
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-08-15 05:36:59 +0100

    Basic annotations - concat

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,4 +2,5 @@
 
 
 def add(a: float, b: float) -> float:
+    """Return the sum of a + b"""
     return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return a concatenated string"""
+    return str1 + str2
