895594d4902b27ca05529e0afffe54fc286e492d
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-08-16 03:09:24 +0100

    Update doc comments for element length function

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """This module contains an annotated function that
-returns info about an iterable"""
+returns info about a sequence"""
 from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Return info about a sequence"""
     return [(i, len(i)) for i in lst]
