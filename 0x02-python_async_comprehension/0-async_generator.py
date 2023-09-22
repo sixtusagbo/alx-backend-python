5a87629cd8ccbfff9e74cd62b884b8ac60a114dc
Sixtus Miracle Agbo <Sixtus Agbo>
2023-09-22 02:34:12 +0000

    Generate floats instead

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,8 @@ from typing import Generator
 import random
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
+    """Yields random integer between 0 and 10"""
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
