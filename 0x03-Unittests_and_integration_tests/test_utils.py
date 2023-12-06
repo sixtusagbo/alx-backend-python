19118379b2b0a88895a815ea8b4ddc2391161b94
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-12-06 22:55:25 +0000

    Test that the exception message is as expected

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,3 +19,17 @@ class TestAccessNestedMap(TestCase):
         """Ensure that this method returns a value from the nested map given"""
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected)
+
+    @parameterized.expand(
+        [
+            ({}, ("a",), "a"),
+            ({"a": 1}, ("a", "b"), "b"),
+        ]
+    )
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """Test that a `KeyError` is raised"""
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+
+        message = str(cm.exception)
+        self.assertEqual(message[1:-1], expected)
